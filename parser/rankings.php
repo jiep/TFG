29141953b6c3ef03d7498d5ecd3220685d20188c
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-01 12:51:55 +0100

    Actualizado parser

--- parser/rankings.php
+++ parser/rankings.php
@@ -1,8 +1,10 @@
 <?php
 
 require("simple_html_dom.php");
+require_once("conexion.php");
 
 $N_JORNADAS = 20;
+$TEMPORADA = "2014/2015";
 
 echo "Descargando datos de lfp.es ... \n\n";
 
@@ -26,10 +28,19 @@ echo "Jornada $i \n-------------------------------- \n";
 
 	for($j = 0; $j < count($locales); $j++){
 
-		if(strpos($resultados[$j]->innertext,'-') !== false){
+		$resultado = $resultados[$j]->innertext;
+		if(strpos($resultado,'-') !== false){
+			$goles = explode("-", $resultado);
+			$equipo_local = $locales[$j]->innertext;
+			$equipo_visitante = $visitantes[$j]->innertext;
+			echo($equipo_local . " " . $goles[0] . "-" . $goles[1] . " " . $equipo_visitante . "\n");
 
-			$goles = explode("-", $resultados[$j]->innertext);
-			echo($locales[$j]->innertext . " " . $goles[0] . "-" . $goles[1] . " " . $visitantes[$j]->innertext . "\n");
+			$db_query = sprintf("INSERT INTO partidos(temporada, jornada, equipo_local, equipo_visitante, goles_local, goles_visitante) VALUES ('%s', '%u','%s','%s','%u','%u')", $TEMPORADA, $i, $equipo_local, $equipo_visitante, $goles[0], $goles[1]);
+			$resultado = mysql_query($db_query);
+
+			if(!$resultado){
+				echo mysql_error() . "\n";
+			}
 
 		}	
 
@@ -40,5 +51,7 @@ echo "--------------------------------\n";
 
 
 curl_close($ch);
+
+require_once("cerrar-conexion.php");
 
 ?>
