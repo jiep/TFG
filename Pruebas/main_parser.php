ce6fa436bb5f5766e644d76b9fe90e775e4acdbe
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-16 09:49:43 +0100

    Prueba desde Atom

--- Pruebas/main_parser.php
+++ Pruebas/main_parser.php
@@ -3,9 +3,6 @@
 // Requerimos el parser.php
 require_once("parser.php");
 
-
-
-
 //Llamamos al parser con la página especificada
 	parser("http://en.wikipedia.org/wiki/2012%E2%80%9313_La_Liga");
 ?>
