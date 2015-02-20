b3895fd370b504582e6a423b2ff9e11d3bd56a71
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-20 20:23:09 +0100

    Modificado grafo de competitividad

--- rankings/RankingCollection.php
+++ rankings/RankingCollection.php
@@ -28,17 +28,30 @@ class RankingCollection {
 
   function calculateCompetitivityGraph(){
 
-    //$elements = $this->rankings->get(0)->getRanking();
-    $elements = null;
-    $n = count($elements);
+    $elements = $this->rankings[0];
+    //$elements = null;
+    $n = $elements->getLength();
     $adjacencyMatrix = array();
 
-    foreach($this->rankings as $ranking){
-      for($i = 0; $i < $ranking->getLength() - 1; $i++){
-        for($j = 0; $j < $ranking->getLength() - 1 - $i; $j++){
-          if($ranking->get($j) < $ranking->get($j+1)){
-            $this->adjacencyMatrix[$i][$j]++;
-            $this->adjacencyMatrix[$j][$i]++;
+    for($k = 0;$k<=count($this->rankings)-2;$k++){
+      $k1 = $this->get($k);
+      $k2 = $this->get($k+1);
+      print_r($k1);
+      print_r($k2);
+      for($i = 0; $i < $n - 1; $i++){
+        for($j = 0; $j < $n - 1 - $i; $j++){
+          $pos_i_r1 = $k1->get($elements->getRanking()[$i]);
+          $pos_j_r1 = $k1->get($elements[$j]);
+          $pos_i_r2 = $k2->get($elements[$i]);
+          $pos_j_r2 = $k2->get($elements[$j]);
+          $sign_r1 = gmp_sign($pos_i_r1 - $pos_j_r1);
+          $sign_r2 = gmp_sign($pos_i_r2 - $pos_j_r2);
+          echo "Signo r1: " . $sign_r1 . "\n";
+          echo "Signo r2: " . $sign_r2 . "\n";
+
+          if($sign_r1 !== $sign_r2){
+              $adjacencyMatrix[$i][$j]++;
+              $adjacencyMatrix[$j][$i]++;
           }
         }
       }
