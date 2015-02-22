7f9395e2627cb6b3658ff28f2fb3527b6f62a53e
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-22 13:52:32 +0100

    Corregidos errores

--- rankings/EvolutiveCompetitivityGraph.php
+++ rankings/EvolutiveCompetitivityGraph.php
@@ -25,14 +25,14 @@ class EvolutiveCompetitivityGraph extends Graph {
       $nodes[$i] = array("data" => array("id" => $this->elements->get($i)));
       for($j = 0; $j < $i; $j++){
         if($this->adjacencyMatrix[$i][$j] !== 0){
-          $edges[$count] = array("data" => array("source" => $i, "target" => $j, "weight" => $this->adjacencyMatrix[$i][$j]));
+          $edges[$count] = array("data" => array("source" => $this->elements->get($i), "target" => $this->elements->get($j), "weight" => $this->adjacencyMatrix[$i][$j]));
           $count++;
         }
       }
     }
 
 
-    return json_encode(array("nodes" => $nodes, "edges" => $edges));
+    return json_encode(array("elements" => array("nodes" => $nodes, "edges" => $edges)));
 
   }
 
