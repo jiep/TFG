c4b9883b6388b08e0e1d41e5d2fe58609d0b4688
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-26 14:15:02 +0100

    Corregido error

--- parser/datos_competitividad.php
+++ parser/datos_competitividad.php
@@ -10,34 +10,25 @@ require_once("connection.inc.php");
 function datos_competitividad($temporada){
 
   $con = new Connection (DB_HOST,DB_PORT,DB_USERNAME,DB_PASSWORD,DB_NAME);
-
   $con->connect();
-
   $con->selectDatabase();
-
   $num_jorn = $con->query("SELECT MAX(jornada) as MAX from rankings where temporada = \"$temporada\" ")[1]["MAX"];
-
   $ranking_col = array();
 
   for($i=1;$i<=$num_jorn;$i++){
-
     $datos = $con->query("SELECT equipo from rankings where temporada = \"$temporada\" and jornada = $i ORDER BY posicion");
     $ranking = new Ranking();
-    foreach($datos as $equipo ){
+    foreach($datos as $equipo){
       if (array_key_exists('equipo', $equipo)){
         $ranking->add($equipo["equipo"]);
       }
     }
-
     $ranking_col[$i-1]=$ranking;
   }
   $rankings = new RankingCollection($ranking_col);
   $con->close();
-  $json = $rankings->calculateEvolutiveCompetitivityGraph()->exportAsCytoscapeJSON();
-  return $json;
+  return($rankings->calculateEvolutiveCompetitivityGraph()->exportAsCytoscapeJSON());
 }
-
-
 
 
 echo(datos_competitividad('1928/1929'));
