0a6f2d7f9c332bcd63416a81b5bff80f1c64b136
José Ignacio Escribano <jiep12345@hotmail.com>
2015-02-26 10:12:27 +0100

    Corregidos errores en datos_competitividad. TODO: Exportar como JSON.

--- parser/datos_competitividad.php
+++ parser/datos_competitividad.php
@@ -3,13 +3,11 @@
 include("autoload.php");
 include("../rankings/Ranking.php");
 include("../rankings/RankingCollection.php");
-
-
+include("../rankings/Graph.php");
+include("../rankings/EvolutiveCompetitivityGraph.php");
 require_once("connection.inc.php");
 
-function datos_competitividad($temporada,$jornada){
-
-  $ranking = new Ranking();
+function datos_competitividad($temporada){
 
   $con = new Connection (DB_HOST,DB_PORT,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
@@ -17,28 +15,31 @@ function datos_competitividad($temporada,$jornada){
 
   $con->selectDatabase();
 
-  $num_jorn = $con->query("SELECT MAX(jornada) from rankings where temporada = \"$temporada\");
+  $num_jorn = $con->query("SELECT MAX(jornada) as MAX from rankings where temporada = \"$temporada\" ")[1]["MAX"];
 
   $ranking_col = array();
 
   for($i=1;$i<=$num_jorn;$i++){
 
-    $datos = $con->query("SELECT equipo from rankings where temporada = \"$temporada\" and jornada = $i");
+    $datos = $con->query("SELECT equipo from rankings where temporada = \"$temporada\" and jornada = $i ORDER BY posicion");
+    $ranking = new Ranking();
     foreach($datos as $equipo ){
-      if (array_key_exists('equipo', $equipo))
+      if (array_key_exists('equipo', $equipo)){
         $ranking->add($equipo["equipo"]);
+      }
     }
 
-    $ranking_col[$i]=$ranking;
+    $ranking_col[$i-1]=$ranking;
   }
-  $rankings = new RankingCollection($ranking_cs
-  print_r($rankings);
+  $rankings = new RankingCollection($ranking_col);
   $con->close();
+  $json = $rankings->calculateEvolutiveCompetitivityGraph()->exportAsCytoscapeJSON();
+  return $json;
 }
 
 
 
 
-datos_competitividad('1928/1929',1);
+echo(datos_competitividad('1928/1929'));
 
 ?>
