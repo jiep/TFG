ec38b2f63f0e1aa50abe935254fcbd16ff40eb82
José Ignacio Escribano <jiep12345@hotmail.com>
2015-03-20 10:57:01 +0100

    Añadido método para calcular grafo de competitividad

--- rankings/RankingCollection.php
+++ rankings/RankingCollection.php
@@ -67,6 +67,43 @@ class RankingCollection {
 
   }
 
+  function calculateCompetitivityGraph(){
+
+    $elements = $this->rankings[0];
+
+    $n = $elements->getLength();
+    $adjacencyMatrix = array();
+
+    for($i = 0; $i < $n; $i++){
+      for($j = 0; $j < $n; $j++){
+        $adjacencyMatrix[$i][$j] = 0;
+      }
+    }
+
+    for($k = 0;$k < count($this->rankings)-1;$k++){
+      $k1 = $this->get($k);
+      $k2 = $this->get($k+1);
+      for($i = 0; $i < $n; $i++){
+        for($j = 0; $j < $i; $j++){
+          $pos_i_r1 = $k1->getPosition($elements->get($i));
+          $pos_j_r1 = $k1->getPosition($elements->get($j));
+          $pos_i_r2 = $k2->getPosition($elements->get($i));
+          $pos_j_r2 = $k2->getPosition($elements->get($j));
+          $sign_r1 = $this->sign($pos_i_r1 - $pos_j_r1);
+          $sign_r2 = $this->sign($pos_i_r2 - $pos_j_r2);
+
+          if($sign_r1 !== $sign_r2){
+              $adjacencyMatrix[$i][$j] == 0 ? $adjacencyMatrix[$i][$j]++;
+              $adjacencyMatrix[$j][$i] == 0 ? $adjacencyMatrix[$j][$i]++;
+          }
+        }
+      }
+    }
+
+    return new CompetitivityGraph($elements, $adjacencyMatrix, count($this->rankings));
+
+  }
+
 }//Fin de clase
 
 
