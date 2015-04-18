06f109af1288bbecad42ff8f94d8df5ef5a8da61
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-18 12:14:06 +0200

    Añadida ruta de temporada

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,12 +1,16 @@
-var app = angular.module('app', ['chieffancypants.loadingBar', 'restangular', 'ngRoute', 'chart.js']);
+var app = angular.module('app', ['chieffancypants.loadingBar', 'restangular', 'ngRoute', 'chart.js', 'rt.encodeuri']);
 
 app.config(function(cfpLoadingBarProvider) {
   cfpLoadingBarProvider.includeSpinner = true;
 });
 
-app.config(function($routeProvider, RestangularProvider) {
+app.config(function($routeProvider, RestangularProvider, $locationProvider) {
   $routeProvider.
   when('/', {
+    controller: "MainCtrl",
+    templateUrl: 'templates/season.html'
+  }).
+  when('/bbva/:season', {
     controller: "MainCtrl",
     templateUrl: 'templates/season.html'
   }).
@@ -14,25 +18,31 @@ app.config(function($routeProvider, RestangularProvider) {
     redirectTo: '/'
   });
 
+  $locationProvider.html5Mode(true);
+
   RestangularProvider.setBaseUrl('http://localhost/TFG/api/sport/football/bbva');
 });
 
-app.controller("MainCtrl", function(Restangular, $scope) {
+app.controller("MainCtrl", function(Restangular, $scope, $routeParams) {
+  var season = '';
+  if ($routeParams.season) {
+    season = $routeParams.season.replace('-', '/');
+  }
   var resource = Restangular.all("seasons");
   resource.getList().then(function(seasons) {
     $scope.seasons = seasons;
   });
-  resource = Restangular.all("teams");
+  resource = Restangular.all("teams?season=" + season);
   resource.getList().then(function(teams) {
     $scope.teams = teams;
   });
 
-  resource = Restangular.all("clasification");
+  resource = Restangular.all("clasification?season=" + season);
   resource.getList().then(function(clasification) {
     $scope.clasification = clasification;
   });
 
-  var competitivityGraph = Restangular.one("competitivity");
+  var competitivityGraph = Restangular.one("competitivity?season=" + season);
   competitivityGraph.get().then(function(competitivityGraph) {
     $scope.competitivityGraph = competitivityGraph.data;
     $scope.style = competitivityGraph.style;
@@ -42,12 +52,14 @@ app.controller("MainCtrl", function(Restangular, $scope) {
       elements: $scope.competitivityGraph.elements,
       zoom: 1,
       zoomingEnabled: false,
-      layout: {name: 'circle'},
+      layout: {
+        name: 'circle'
+      },
       style: $scope.style
     });
   });
 
-  var chartLine = Restangular.one("chartLine");
+  var chartLine = Restangular.one("chartLine?season=" + season);
   chartLine.get().then(function(chartLine) {
     $scope.chartLine = chartLine;
 
@@ -62,15 +74,15 @@ app.controller("MainCtrl", function(Restangular, $scope) {
     };
   });
 
-  $scope.downloadPDF = function(){
+  $scope.downloadPDF = function() {
 
-    function parseClasification(){
+    function parseClasification() {
       var clasif = Restangular.stripRestangular($scope.clasification);
       var c = [];
       var d = [];
       var headers = ["#", "Equipo", "PG", "PE", "PP", "GF", "GC", "Puntos"];
       d.push(headers);
-      for(var i = 0; i < 20; i++){
+      for (var i = 0; i < 20; i++) {
         c = [];
         c.push(clasif[i].posicion);
         c.push(clasif[i].equipo);
@@ -79,7 +91,8 @@ app.controller("MainCtrl", function(Restangular, $scope) {
         c.push(clasif[i].partidos_perdidos);
         c.push(clasif[i].goles_favor);
         c.push(clasif[i].goles_contra);
-        c.push(clasif[i].puntos);0
+        c.push(clasif[i].puntos);
+        0
         d.push(c);
       }
       return d;
@@ -87,45 +100,48 @@ app.controller("MainCtrl", function(Restangular, $scope) {
 
     var d = new parseClasification();
 
-    var hist = document.getElementById("canvas2").toDataURL("image/png",1);
+    var hist = document.getElementById("canvas2").toDataURL("image/png", 1);
 
     var dd = {
-	     content: [
-         {
-           columns: [
-            { width: '*', text: '' },
-        {
+      content: [{
+          columns: [{
+            width: '*',
+            text: ''
+          }, {
             width: 'auto',
             table: {
-              headerRows : 1,
+              headerRows: 1,
               alignment: 'center',
-              body : d
+              body: d
             }
+          }, {
+            width: '*',
+            text: ''
+          }, ]
         },
-        { width: '*', text: '' },
-    ]
-  },
-    'Cambios de posición',
-    {
-      image: hist,
-			width: 300,
-			height: 300,
-    },
-    'Grafo de competitividad',
-    {
-      image: $scope.cy.png(),
-			width: 300,
-			height: 300,
-    }
-       ]
+        'Cambios de posición', {
+          image: hist,
+          width: 300,
+          height: 300,
+        },
+        'Grafo de competitividad', {
+          image: $scope.cy.png(),
+          width: 300,
+          height: 300,
+        }
+      ]
     }
 
     pdfMake.createPdf(dd).open();
-    };
+  };
 });
 
-app.controller('RadarCtrl', function($scope, Restangular) {
-  var measures = Restangular.one("measures");
+app.controller('RadarCtrl', function($scope, Restangular, $routeParams) {
+  var season = '';
+  if ($routeParams.season) {
+    season = $routeParams.season.replace('-', '/');
+  }
+  var measures = Restangular.one("measures?season" + season);
   measures.get().then(function(measures) {
     $scope.measures = measures;
     $scope.labels = $scope.measures.labels;
@@ -133,12 +149,16 @@ app.controller('RadarCtrl', function($scope, Restangular) {
   });
 });
 
-app.controller('MeasureCtrl', function($scope, Restangular) {
-  var measures = Restangular.one("measures");
+app.controller('MeasureCtrl', function($scope, Restangular, $routeParams) {
+  var season = '';
+  if ($routeParams.season) {
+    season = $routeParams.season.replace('-', '/');
+  }
+  var measures = Restangular.one("measures?season" + season);
   measures.get().then(function(measures) {
     $scope.measuresp = measures;
     $scope.labels = $scope.measuresp.labels_array;
-    $scope.data = [$scope.measuresp.ndd,$scope.measuresp.ncdd,$scope.measuresp.nsd,$scope.measuresp.ncsd];
+    $scope.data = [$scope.measuresp.ndd, $scope.measuresp.ncdd, $scope.measuresp.nsd, $scope.measuresp.ncsd];
     $scope.options = {
       responsive: true,
       animation: false,
