44d0b5721aa062b120cc0ca6cfbb3251ef956d51
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-19 14:23:44 +0200

    Closes #35

---
+++ parser/equipos.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: rankings
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `equipos`
+--
+
+LOCK TABLES `equipos` WRITE;
+/*!40000 ALTER TABLE `equipos` DISABLE KEYS */;
+INSERT INTO `equipos` VALUES (1,'A.D. Almería',''),(2,'Albacete Balomp.',''),(3,'Arenas de Guecho',''),(4,'At. Tetuan',''),(5,'Athletic Club',''),(6,'Atlético Madrid',''),(7,'Burgos C.F.',''),(8,'C. y D. Leonesa',''),(9,'C.D. Alavés',''),(10,'C.D. Alcoyano',''),(11,'C.D. Castellón',''),(12,'C.D. Europa',''),(13,'C.D. Logroñés',''),(14,'C.D. Málaga',''),(15,'C.D. Numancia',''),(16,'C.D. Sabadell',''),(17,'C.D. Tenerife',''),(18,'C.F. Extremadura',''),(19,'CA Osasuna',''),(20,'Cádiz C.F.',''),(21,'Club Gimnàstic',''),(22,'Condal',''),(23,'Córdoba C.F.',''),(24,'Elche CF',''),(25,'FC Barcelona',''),(26,'Getafe CF',''),(27,'Granada CF',''),(28,'Hércules C.F.',''),(29,'Levante UD',''),(30,'Málaga CF',''),(31,'Mérida C.P.',''),(32,'Pontevedra C.F.',''),(33,'R.C. Deportivo',''),(34,'R.C.D. Mallorca',''),(35,'R.C.R. de Huelva',''),(36,'Rayo Vallecano',''),(37,'RC Celta de Vigo',''),(38,'RCD Espanyol',''),(39,'Real Betis',''),(40,'Real Burgos C.F.',''),(41,'Real Jaén C.F.',''),(42,'Real Madrid CF',''),(43,'Real Murcia C.F.',''),(44,'Real Oviedo C.F.',''),(45,'Real Racing Club',''),(46,'Real S. de Gijón',''),(47,'Real Sociedad',''),(48,'Real Unión Club',''),(49,'Real Valladolid',''),(50,'Real Zaragoza CD',''),(51,'S.D. Compostela',''),(52,'SD Eibar',''),(53,'Sevilla FC',''),(54,'U.D. Las Palmas',''),(55,'U.D. Salamanca',''),(56,'U.E. Lleida',''),(57,'UD Almería',''),(58,'Valencia CF',''),(59,'Villarreal CF',''),(60,'Xerez C.D.','');
+/*!40000 ALTER TABLE `equipos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-19 14:20:24
