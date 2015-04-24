54147eebb06195a2b1645b1c82e278caef870e83
javijdp <javierjimenez11@hotmail.com>
2015-04-24 09:36:26 +0200

    Close #38

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -211,7 +211,6 @@ app.controller("LocalCtrl", function(Restangular, $location, $scope) {
     $scope.options = {
       responsive: true,
       animation: false,
-      bezierCurve: false,
       datasetFill: false
     };
   });
@@ -230,7 +229,41 @@ app.controller("AwayCtrl", function(Restangular, $location, $scope) {
     $scope.options = {
       responsive: true,
       animation: false,
-      bezierCurve: false,
+      datasetFill: false
+    };
+  });
+});
+
+
+app.controller("GoalsLocalCtrl", function(Restangular, $location, $scope) {
+  var team = $location.url().split("/")[3];
+
+  var team_stats = Restangular.one("team/" + team);
+  team_stats.get().then(function(stats) {
+    $scope.team_stats = stats;
+    $scope.labels = ["Goles a favor", "Goles en contra"];
+    $scope.data = [parseFloat($scope.team_stats.local_goals_favor), parseFloat($scope.team_stats.local_goals_contra)];
+    $scope.options = {
+      responsive: true,
+      animation: false,
+      datasetFill: false
+    };
+  });
+
+
+});
+
+app.controller("GoalsAwayCtrl", function(Restangular, $location, $scope) {
+  var team = $location.url().split("/")[3];
+
+  var team_stats = Restangular.one("team/" + team);
+  team_stats.get().then(function(stats) {
+    $scope.team_stats = stats;
+    $scope.labels = ["Goles a favor", "Goles en contra"];
+    $scope.data = [parseFloat($scope.team_stats.away_goals_favor), parseFloat($scope.team_stats.away_goals_contra)];
+    $scope.options = {
+      responsive: true,
+      animation: false,
       datasetFill: false
     };
   });
