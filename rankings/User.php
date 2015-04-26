d2de2384ef88626d346ae5ec594649e3100c236e
José Ignacio Escribano <jiep12345@hotmail.com>
2015-04-26 19:10:52 +0200

    Añadidos getters en User #9

--- rankings/User.php
+++ rankings/User.php
@@ -1,34 +1,55 @@
 <?php
 
-class User {
+class User
+{
+  public $username;
+    public $password;
+    public $email;
+    public $apiKey;
 
-  var $username;
-  var $password;
-  var $email;
-  var $apiKey;
+    public function __construct($username, $password, $email)
+    {
+        if ($this->isValidEmail($email)) {
+            $this->email = $email;
+        }
 
-  public function __construct($username, $password, $email){
-    if($this->isValidEmail($email)){
-      $this->email = $email;
+        $this->username = $username;
+        $this->password = $this->getHashPassword($password);
+        $this->apiKey = $this->generateApiKey();
     }
 
-    $this->username = $username;
-    $this->password = $this->getHashPassword($password);
-    $this->apiKey = $this->generateApiKey();
-  }
+    public function generateApiKey()
+    {
+        return md5(uniqid(rand(), true));
+    }
 
-  public function generateApiKey(){
-    return md5(uniqid(rand(), true));
-  }
+    private function getHashPassword($password)
+    {
+        return crypt($password, '$2a'.substr(sha1(mt_rand()), 0, 30));
+    }
 
-  private function getHashPassword($password){
-    return crypt($password, '$2a' . substr(sha1(mt_rand()), 0, 30));
-  }
+    private function isValidEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
 
-  private function isValidEmail($email){
-    print_r(filter_var($email, FILTER_VALIDATE_EMAIL));
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-  }
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
 }
-
-?>
