b6fec81d51f4e36156376b5c3922259ab487f100
javijdp <javierjimenez11@hotmail.com>
2015-11-25 23:22:28 +0100

    Añadidas funciones que combinan probabilidad de las tendencias

--- prediccion/tendencia/tendencia.php
+++ prediccion/tendencia/tendencia.php
@@ -3,7 +3,6 @@
 include '../../parser/autoload.php';
 require_once '../../parser/connection.inc.php';
 
-define("JOR",37);
 
 function getLocal($temp,$jorn,$team){
 
@@ -29,7 +28,7 @@ function getLocal($temp,$jorn,$team){
           if($result[0]['goles_local'] > $result[0]['goles_visitante']) {$local[$cont]=0;}
           else if($result[0]['goles_local'] == $result[0]['goles_visitante']) {$local[$cont]=1;}
           else {$local[$cont]=2;}
-  echo $jorn;
+
           $cont++;
         }
 
@@ -65,7 +64,7 @@ function getVisitante($temp,$jorn,$team){
           if($result[0]['goles_local'] > $result[0]['goles_visitante']) {$visitante[$cont]=0;}
           else if($result[0]['goles_local'] == $result[0]['goles_visitante']) {$visitante[$cont]=1;}
           else {$visitante[$cont]=2;}
-  echo $jorn;
+
           $cont++;
         }
 
@@ -77,6 +76,54 @@ function getVisitante($temp,$jorn,$team){
 
 }
 
-print_r(getLocal(" ",38,"Real Madrid CF"));
-print_r(getVisitante(" ",38,"Real Madrid CF"));
+function probability($tend){
+  $prob=array();
+  $gana_local=0;
+  $tie=0;
+  $gana_visitante=0;
+
+  for($i=0;$i<sizeOf($tend);$i++){
+    if($tend[$i]==0){$gana_local+=memory($i);}
+    else if($tend[$i]==1){$tie+=memory($i);}
+    else {$gana_visitante+=memory($i);}
+  }
+
+  $den=$gana_local+$tie+$gana_visitante;
+  $prob["gana_local"]= $gana_local/$den;
+  $prob["empate"]= $tie/$den;
+  $prob["gana_visitante"]= $gana_visitante/$den;
+
+  return $prob;
+}
+
+function memory($position){
+
+  return 1;
+}
+
+function combine($prob_local,$prob_visitante){
+  $prob = array();
+
+  $prob["gana_local"]=
+  $prob_local["gana_local"]*$prob_visitante["gana_local"]+(1/2)*$prob_local["empate"]*$prob_visitante["gana_local"]+(1/4)*$prob_local["gana_visitante"]*$prob_visitante["gana_local"]+
+  (1/2)*$prob_local["gana_local"]*$prob_visitante["empate"]+(1/6)*$prob_local["gana_visitante"]*$prob_visitante["empate"]+
+  (1/4)*$prob_local["gana_local"]*$prob_visitante["gana_visitante"]+(1/6)*$prob_local["empate"]*$prob_visitante["gana_visitante"];
+
+  $prob["empate"]=
+  (1/3)*$prob_local["empate"]*$prob_visitante["gana_local"]+(1/2)*$prob_local["gana_visitante"]*$prob_visitante["gana_local"]+
+  (1/3)*$prob_local["gana_local"]*$prob_visitante["empate"]+$prob_local["empate"]*$prob_visitante["empate"]+(1/2)*$prob_local["gana_visitante"]*$prob_visitante["empate"]+
+  (1/2)*$prob_local["gana_local"]*$prob_visitante["gana_visitante"]+(1/2)*$prob_local["empate"]*$prob_visitante["gana_visitante"];
+
+  $prob["gana_visitante"]=
+  (1/6)*$prob_local["empate"]*$prob_visitante["gana_local"]+(1/4)*$prob_local["gana_visitante"]*$prob_visitante["gana_local"]+
+  (1/6)*$prob_local["gana_local"]*$prob_visitante["empate"]+(1/3)*$prob_local["gana_visitante"]*$prob_visitante["empate"]+
+  (1/4)*$prob_local["gana_local"]*$prob_visitante["gana_visitante"]+(1/3)*$prob_local["empate"]*$prob_visitante["gana_visitante"]+$prob_local["gana_visitante"]*$prob_visitante["gana_visitante"];
+
+  return $prob;
+
+}
+
+//print_r(getVisitante(" ",38,"Real Madrid CF"));
+//print_r(probability(getVisitante(" ",38,"Real Madrid CF")));
+print_r(combine(probability(getLocal(" ",37,"Real Madrid CF")),probability(getVisitante(" ",37,"Getafe CF"))));
 ?>
