8335a058263ac5f4e4aa73e1a28c1421fae62514
javijdp <javierjimenez11@hotmail.com>
2015-12-01 23:20:36 +0100

    Añadido scipt que calcula valores de lambda

---
+++ prediccion/combinacion/calcular_lambda.php
@@ -0,0 +1,54 @@
+<?php
+include '../../parser/autoload.php';
+include 'comb_convexa.php';
+require_once '../../parser/connection.inc.php';
+function calcularLambda(){
+
+    $season="2014/2015";
+    $fixture=38;
+
+    //Resultados
+    try {
+        $dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USERNAME, DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $dbh->prepare("select * from partidos where temporada = \"$season\" and jornada = $fixture;");
+        $sql->execute();
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    //print_r($result);
+
+    //Predicción
+    $curl = curl_init("http://localhost/TFG/web/api/sport/football/bbva/prediction?season=".$season."&fixture=".$fixture);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $pred = json_decode($response, true);
+    //print_r($pred);
+
+    $lambda = 0;
+
+    while($lambda<=1){
+    	$success=0;
+			$conj = conjugarconlambda($pred["results1"],$pred["results2"],$lambda);
+			for($i=0;$i<10;$i++){
+		      if($result[$i]["goles_local"] > $result[$i]["goles_visitante"]) {$winner=0;}
+		      else if($result[$i]["goles_local"] == $result[$i]["goles_visitante"]) {$winner=1;}
+		      else {$winner=2;}
+
+		      if($conj[$i]["local_win"]>$conj[$i]["tie"] && $conj[$i]["local_win"] > $conj[$i]["visitor_win"]){$winner_p=0;}
+		      else if($conj[$i]["tie"]>$conj[$i]["local_win"] && $conj[$i]["tie"] > $conj[$i]["visitor_win"]){$winner_p=1;}
+		      else {$winner_p=2;}
+
+		      if($winner==$winner_p){$success++;}
+			}
+
+			echo "Para lambda = " . $lambda . " el numero de aciertos es " . $success . ".\n";
+			$lambda = $lambda + 0.1;
+
+    }
+
+}
+calcularLambda();
+?>
--- prediccion/combinacion/comb_convexa.php
+++ prediccion/combinacion/comb_convexa.php
@@ -1,12 +1,12 @@
 <?php
 require_once '../../parser/connection.inc.php';
 
-define("LAMBDA",0);
+define("LAMBDA",1/5);
 
 function conjugar($array_interp,$array_tend){
-	
+
 	$conj = array();
-	$lambda = LAMBDA;	
+	$lambda = LAMBDA;
 
 	for($i=0;$i<10;$i++){
 		$conj[$i]["local_team"]= $array_interp[$i]["local_team"];
@@ -14,20 +14,20 @@ function conjugar($array_interp,$array_tend){
 		$conj[$i]["local_win"]=	$lambda * $array_interp[$i]["local_win"] + (1 - $lambda) * $array_tend[$i]["local_win"];
 		$conj[$i]["tie"]= $lambda * $array_interp[$i]["tie"] + (1 - $lambda) * $array_tend[$i]["tie"];
 		$conj[$i]["visitor_win"]= $lambda * $array_interp[$i]["visitor_win"] + (1 - $lambda) * $array_tend[$i]["visitor_win"];
-	}	
+	}
 
 	return $conj;
 }
 
 function conjugarconlambda($array_interp,$array_tend,$lambda){
-	
-	$conj = array();	
+
+	$conj = array();
 
 	for($i=0;$i<10;$i++){
 		$conj[$i]["local_win"]=	$lambda * $array_interp[$i]["local_win"] + (1 - $lambda) * $array_tend[$i]["local_win"];
 		$conj[$i]["tie"]= $lambda * $array_interp[$i]["tie"] + (1 - $lambda) * $array_tend[$i]["tie"];
 		$conj[$i]["visitor_win"]= $lambda * $array_interp[$i]["visitor_win"] + (1 - $lambda) * $array_tend[$i]["visitor_win"];
-	}	
+	}
 
 	return $conj;
 }
